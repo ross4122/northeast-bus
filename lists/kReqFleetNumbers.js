bb6bf11a0f7bb29c8677d28f3293688f9eee9006
ross4122 <48660479+ross4122@users.noreply.github.com>
2026-04-16 21:28:30 +0100

    Add files via upload

--- lists/kReqFleetNumbers.js
+++ lists/kReqFleetNumbers.js
@@ -3,13 +3,8 @@ const kReqIconFleetNumbers = new Set([
 
 	// GO NORTH EAST
 	"GNE|6045",
-	"GNE|6074",
-	"GNE|6362",
-	"GNE|6819",
+
 	// STAGECOACH NORTH EAST
-	"",
-	"",
-	"",
 	"stagecoach|28003",
 	"stagecoach|28018"
 ]);
